bcfd105cbb33df9f5fefd09c575a7dd72a035354
Robin Norwood <rnorwood@hashicorp.com>
2020-10-23 12:37:24 -0500

    Quote variable names

--- modules/aws-instance/variables.tf
+++ modules/aws-instance/variables.tf
@@ -1,24 +1,24 @@
-variable instance_count {
+variable "instance_count" {
   description = "Number of EC2 instances to deploy"
   type        = number
 }
 
-variable instance_type {
+variable "instance_type" {
   description = "Type of EC2 instance to use"
   type        = string
 }
 
-variable subnet_ids {
+variable "subnet_ids" {
   description = "Subnet IDs for EC2 instances"
   type        = list(string)
 }
 
-variable security_group_ids {
+variable "security_group_ids" {
   description = "Security group IDs for EC2 instances"
   type        = list(string)
 }
 
-variable tags {
+variable "tags" {
   description = "Tags for instances"
   type        = map
   default     = {}
