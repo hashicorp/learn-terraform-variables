1fa04d53b347af75841bbeb4023b35f0b1413878
Izek Chen <mz00000102@gmail.com>
2020-12-26 16:52:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      version = "3.22.0"
+      source  = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
-  version = "3.22.0"
+
   region  = "us-west-2"
 }
 
